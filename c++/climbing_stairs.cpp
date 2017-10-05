c196f24f7a784802b99012cc1cbfedaac99273a8
zsmountain <zsmountain27@gmail.com>
2017-10-05 00:18:57 -0400

    70. Climbing Stairs

---
+++ c++/climbing_stairs.cpp
@@ -0,0 +1,38 @@
+/*
+You are climbing a stair case. It takes n steps to reach to the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Note: Given n will be a positive integer.
+*/
+
+/*
+ * dp[i] = dp[i-1] + dp[i-2]
+*/
+
+#include "helper.h"
+
+class Solution {
+public:
+    int climbStairs(int n) {
+        if (n == 0) {
+            return 0;
+        }
+        if (n == 1) {
+            return 1;
+        }
+        int dp[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for (int i = 2; i < n; ++i) {
+            dp[i] = dp[i - 1] + dp[i - 2];
+        }
+        return dp[n - 1];
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.climbStairs(1) << endl;
+    return 0;
+}
