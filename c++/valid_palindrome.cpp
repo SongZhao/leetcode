f7803fb1a6b72225f421f8191826b25f5b0730be
Song Zhao <zsmountain27@gmail.com>
2017-10-25 20:30:14 -0400

    125. Valid Palindrome

---
+++ c++/valid_palindrome.cpp
@@ -0,0 +1,51 @@
+/*
+Given a string, determine if it is a palindrome, considering only alphanumeric characters and ignoring cases.
+
+For example,
+"A man, a plan, a canal: Panama" is a palindrome.
+"race a car" is not a palindrome.
+
+Note:
+Have you consider that the string might be empty? This is a good question to ask during an interview.
+
+For the purpose of this problem, we define empty string as valid palindrome.
+
+*/
+
+/*
+ *
+*/
+
+#include "helper.h"
+
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int start = 0, end = s.size() - 1;
+        while (end > start) {
+            if (!isalnum(s[start])) {
+                start++;
+                continue;
+            }
+            if (!isalnum(s[end])) {
+                end--;
+                continue;
+            }
+            if (tolower(s[start]) != tolower(s[end])) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+        return true;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.isPalindrome("A man, a plan, a canal: Panama") << endl;
+    cout << s.isPalindrome("race a car") << endl;
+    cout << s.isPalindrome("") << endl;
+    cout << s.isPalindrome("1") << endl;
+    return 0;
+}
