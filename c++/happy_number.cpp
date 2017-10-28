81faca9729d9e8ca5198e7b4bf537a4abbfb62b2
Song Zhao <zsmountain27@gmail.com>
2017-10-28 12:51:00 -0400

    202. Happy Number

---
+++ c++/happy_number.cpp
@@ -0,0 +1,50 @@
+/*
+Write an algorithm to determine if a number is "happy".
+
+A happy number is a number defined by the following process: Starting with any positive integer, replace the number by the sum of the squares of its digits, and repeat the process until the number equals 1 (where it will stay), or it loops endlessly in a cycle which does not include 1. Those numbers for which this process ends in 1 are happy numbers.
+
+Example: 19 is a happy number
+
+12 + 92 = 82
+82 + 22 = 68
+62 + 82 = 100
+12 + 02 + 02 = 1
+*/
+
+/*
+ * Use similar algorithm in detecting cycle linked list to detect cycle. Hash may also work.
+*/
+
+#include "helper.h"
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        int fast = next(next(n)), slow = next(n);
+        while (true) {
+            if (slow == 1) {
+                return true;
+            }
+            if (fast == slow) {
+                return false;
+            }
+            slow = next(slow);
+            fast = next(next(fast));
+        }
+    }
+
+    int next(int n) {
+        int res = 0;
+        while (n > 0) {
+            res += (n % 10) * (n%10);
+            n /= 10;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.isHappy(0) << endl;
+    return 0;
+}
