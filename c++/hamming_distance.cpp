d1539843ab8f6623edb528b8ba114b3ad001fb16
Song Zhao <zsmountain27@gmail.com>
2017-11-07 18:54:49 -0500

    461. Hamming Distance

---
+++ c++/hamming_distance.cpp
@@ -0,0 +1,47 @@
+/*
+The Hamming distance between two integers is the number of positions at which the corresponding bits are different.
+
+Given two integers x and y, calculate the Hamming distance.
+
+Note:
+0 ≤ x, y < 2^31.
+
+Example:
+
+Input: x = 1, y = 4
+
+Output: 2
+
+Explanation:
+1   (0 0 0 1)
+4   (0 1 0 0)
+       ↑   ↑
+
+The above arrows point to positions where the corresponding bits are different.
+*/
+
+/*
+ *
+ */
+
+#include "helper.h"
+
+class Solution {
+public:
+    int hammingDistance(int x, int y) {
+        int res = 0;
+        while (x || y) {
+            res += (x & 1) != (y & 1);
+            x >>= 1;
+            y >>= 1;
+        }
+        return res;
+    }
+};
+
+int main() {
+    Solution s;
+    cout << s.hammingDistance(1, 4) << endl;
+
+    return 0;
+}
