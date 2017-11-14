80ea81c9377d268d4266396fa268978c731d499b
Song Zhao <zsmountain27@gmail.com>
2017-11-13 19:24:19 -0500

    628. Maximum Product of Three Numbers

---
+++ c++/maximum_product_of_three_numbers.cpp
@@ -0,0 +1,37 @@
+/*
+Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+Example 1:
+Input: [1,2,3]
+Output: 6
+Example 2:
+Input: [1,2,3,4]
+Output: 24
+Note:
+The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+*/
+
+/*
+ * Getting top 3 max and top 2 min in one loop also works
+*/
+
+#include "helper.h"
+
+
+class Solution {
+public:
+    int maximumProduct(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        int n = nums.size();
+        return max(nums[n-1] * nums[n-2] * nums[n-3], nums[n-1] * nums[0] * nums[1]);
+    }
+};
+
+int main() {
+    Solution s;
+    vector<int> nums = {1, 2, -3, -4};
+    cout << s.maximumProduct(nums) << endl;
+
+    return 0;
+}
